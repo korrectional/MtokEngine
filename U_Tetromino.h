f65209129d74170e90054b78312d471f3c4c3ed1
Korrectional <156260369+korrectional@users.noreply.github.com>
2025-05-18 19:39:39 -0400

    Tetromino v1

--- U_Tetromino.h
+++ U_Tetromino.h
@@ -25,7 +25,30 @@ GameObject* tetrisBlocks[] = {
     instantiate("block"), instantiate("block"), instantiate("block"), instantiate("block")
 };
 
+/*
 
++---------------------------------------------+
+|      This script is the official winner     |
+|         of the Mtok "spaggeti" award!       |
+|                                             |
+|                ___________                  |
+|               '._==_==_=_.'                 |
+|               .-\:      /-.                 |
+|              | (|:.     |) |                |
+|               '-|:.     |-'                 |
+|                 \::.    /                   |
+|                  '::. .'                    |
+|                    ) (                      |
+|                  _.' '._                    |
+|                 `"""""""`                   |
+|                                             |
++---------------------------------------------+
+
+*/
+
+float randomVal() {
+    return (float)rand() / RAND_MAX;
+}
 
 void Tetris_userStart() {
     pov = 3;
@@ -47,8 +70,78 @@ int Tetris_overlayMovement = 0;
 
 int currentBlock[] = {9, 2};
 int oldCurrentBlock[] = {0, 0};
-int currentBlockId = 0;
 bool currentBlockMoving = false;
+int currentBlockId = 0;
+/*
+================
+id: 0
++----+
+|    |
++----+
+tetris[9][2]
+
+================
+id: 1
++----+----+
+|    |    |
++----+----+
+tetris[9][2]
+tetris[9][3]
+
+================
+id: 2
++----+----+
+|    |    |
++----+----+
+|    |    |
++----+----+
+tetris[9][2]
+tetris[9][3]
+tetris[8][2]
+tetris[8][3]
+
+================ IDRN
+id: 3
++----+
+|    |
+-----+----+
+     |    |
+     +----+
+tetris[9][2]
+tetris[9][3]
+tetris[8][2]
+tetris[8][3]
+
+================*/
+
+// checks if row was completed, and if yes delets it and updates the array
+void checkRow(){
+    for(int i = 0; i < 10; i++){
+        bool rowFull = true;
+        for(int j = 0; j < 5; j++){
+            if(tetris[i][j] == 0){
+                rowFull = false;
+            }
+        }
+        
+        if(rowFull){
+            // update array
+            for(int z = i; z < 9; z++){
+                for(int w = 0; w < 5; w++){
+                    tetris[z][w] = tetris[z+1][w];
+                }
+            }
+            // clear top row
+            for(int j = 0; j < 5; j++){
+                tetris[9][j] = 0;
+            }
+            for(int i = 0; i < 40; i++) { // refresh position of all gameobjects
+                tetrisBlocks[i]->setPosition(50, 50, +20);
+            }
+        }
+    }
+}
+
 
 int iterations = 0;
 int frames = 0; // count of how many frames passed
@@ -59,7 +152,6 @@ float frameChange = 0.4; // how much to decrease frameTime as each frame takes l
 bool pressed1 = false; // player wont be able to press the button continuously
 bool pressed2 = false;
 
-
 void Tetris_gameLoop() {
     if (!playing) return;
     Tetris_overlayMovement++;
@@ -67,10 +159,22 @@ void Tetris_gameLoop() {
 
 
     if(!currentBlockMoving){
+        
+        // check if a row was completed
+        checkRow();
+
+
+        // select new object id
+        currentBlockId = (int)((randomVal()-0.001)*3.0);
+        // spawn new object
         if(tetris[9][2] == 0){
             currentBlock[0] = 9;currentBlock[1] = 2;
             if(currentBlockId == 0){
                 tetris[9][2] = 1;
+            }
+            if(currentBlockId == 1){
+                tetris[9][2] = 1;
+                tetris[9][3] = 1;
             }
         }
         currentBlockMoving = true;
@@ -83,9 +187,44 @@ void Tetris_gameLoop() {
             //frameChange += 0.4;
             Serial.println(String(frameTime));
             if(currentBlockId == 0){
-                if(tetris[currentBlock[0]-1][currentBlock[1]] == 0){
+                if(tetris[currentBlock[0]-1][currentBlock[1]] == 0){ // gravity
                     tetris[currentBlock[0]][currentBlock[1]] = 0;
                     tetris[currentBlock[0]-1][currentBlock[1]] = 1;
+                    
+                    currentBlock[0] = currentBlock[0]-1;
+                    currentBlock[1] = currentBlock[1];
+                }
+                else{
+                    currentBlockMoving = false;
+                }
+                
+            }
+            if(currentBlockId == 1){
+                if(tetris[currentBlock[0]-1][currentBlock[1]] == 0 && tetris[currentBlock[0]-1][currentBlock[1]+1] == 0){ // gravity
+                    tetris[currentBlock[0]][currentBlock[1]] = 0;
+                    tetris[currentBlock[0]][currentBlock[1]+1] = 0;
+                    tetris[currentBlock[0]-1][currentBlock[1]] = 1;
+                    tetris[currentBlock[0]-1][currentBlock[1]+1] = 1;
+                    
+                    currentBlock[0] = currentBlock[0]-1;
+                    currentBlock[1] = currentBlock[1];
+                }
+                else{
+                    currentBlockMoving = false;
+                }
+                
+            }
+            if(currentBlockId == 2){
+                if(tetris[currentBlock[0]-2][currentBlock[1]] == 0 && tetris[currentBlock[0]-2][currentBlock[1]+1] == 0){ // gravity
+                    tetris[currentBlock[0]][currentBlock[1]] = 0;
+                    tetris[currentBlock[0]][currentBlock[1]+1] = 0;
+                    tetris[currentBlock[0]-1][currentBlock[1]] = 0;
+                    tetris[currentBlock[0]-1][currentBlock[1]+1] = 0;
+                    tetris[currentBlock[0]-1][currentBlock[1]] = 1;
+                    tetris[currentBlock[0]-1][currentBlock[1]+1] = 1;
+                    tetris[currentBlock[0]-2][currentBlock[1]] = 1;
+                    tetris[currentBlock[0]-2][currentBlock[1]+1] = 1;
+
                     
                     currentBlock[0] = currentBlock[0]-1;
                     currentBlock[1] = currentBlock[1];
@@ -109,6 +248,32 @@ void Tetris_gameLoop() {
                     currentBlock[1] = currentBlock[1]-1;        
                 }
             }
+            if(currentBlockId == 1){
+                if(tetris[currentBlock[0]][currentBlock[1]-1] == 0 && currentBlock[1]!=0){
+                    tetris[currentBlock[0]][currentBlock[1]] = 0;
+                    tetris[currentBlock[0]][currentBlock[1]+1] = 0;
+                    tetris[currentBlock[0]][currentBlock[1]-1] = 1;
+                    tetris[currentBlock[0]][currentBlock[1]] = 1;
+                    
+                    currentBlock[0] = currentBlock[0];
+                    currentBlock[1] = currentBlock[1]-1;        
+                }
+            }
+            if(currentBlockId == 2){
+                if(tetris[currentBlock[0]][currentBlock[1]-1] == 0 && tetris[currentBlock[0]-1][currentBlock[1]-1] == 0 && currentBlock[1]!=0){
+                    tetris[currentBlock[0]][currentBlock[1]] = 0;
+                    tetris[currentBlock[0]][currentBlock[1]+1] = 0;
+                    tetris[currentBlock[0]-1][currentBlock[1]] = 0;
+                    tetris[currentBlock[0]-1][currentBlock[1]+1] = 0;
+                    tetris[currentBlock[0]][currentBlock[1]-1] = 1;
+                    tetris[currentBlock[0]][currentBlock[1]] = 1;
+                    tetris[currentBlock[0]-1][currentBlock[1]-1] = 1;
+                    tetris[currentBlock[0]-1][currentBlock[1]] = 1;
+                    
+                    currentBlock[0] = currentBlock[0];
+                    currentBlock[1] = currentBlock[1]-1;        
+                }
+            }
         }
         if(button1 == HIGH && !pressed1){
             pressed1 = true;
@@ -117,6 +282,32 @@ void Tetris_gameLoop() {
                 if(tetris[currentBlock[0]][currentBlock[1]+1] == 0 && currentBlock[1]!=4){
                     tetris[currentBlock[0]][currentBlock[1]] = 0;
                     tetris[currentBlock[0]][currentBlock[1]+1] = 1;
+                    
+                    currentBlock[0] = currentBlock[0];
+                    currentBlock[1] = currentBlock[1]+1;
+                }
+            }
+            if(currentBlockId == 1){
+                if(tetris[currentBlock[0]][currentBlock[1]+2] == 0 && (currentBlock[1]+1)!=4){
+                    tetris[currentBlock[0]][currentBlock[1]] = 0;
+                    tetris[currentBlock[0]][currentBlock[1]+1] = 0;
+                    tetris[currentBlock[0]][currentBlock[1]+1] = 1;
+                    tetris[currentBlock[0]][currentBlock[1]+2] = 1;
+                    
+                    currentBlock[0] = currentBlock[0];
+                    currentBlock[1] = currentBlock[1]+1;
+                }
+            }
+            if(currentBlockId == 2){
+                if(tetris[currentBlock[0]][currentBlock[1]+2] == 0 && tetris[currentBlock[0]-1][currentBlock[1]+2] == 0 && (currentBlock[1]+1)!=4){
+                    tetris[currentBlock[0]][currentBlock[1]] = 0;
+                    tetris[currentBlock[0]][currentBlock[1]+1] = 0;
+                    tetris[currentBlock[0]-1][currentBlock[1]] = 0;
+                    tetris[currentBlock[0]-1][currentBlock[1]+1] = 0;
+                    tetris[currentBlock[0]][currentBlock[1]+1] = 1;
+                    tetris[currentBlock[0]][currentBlock[1]+2] = 1;
+                    tetris[currentBlock[0]-1][currentBlock[1]+1] = 1;
+                    tetris[currentBlock[0]-1][currentBlock[1]+2] = 1;
                     
                     currentBlock[0] = currentBlock[0];
                     currentBlock[1] = currentBlock[1]+1;
